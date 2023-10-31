e7fd7d75153b31f64403ce858a0bb2c09a5cc738
Sia <129706771+0Medsok0@users.noreply.github.com>
2023-10-31 21:41:14 +0700

    Update Виселица.py

--- python/Виселица.py
+++ python/Виселица.py
@@ -1,25 +1,36 @@
 import random
 
-
-list =  ['apple', 'ball', 'cat', 'dog', 'elephant', 'giraffe', 'house', 'tree', 'water', 'mountain', 'river', 'lake', 'cloud', 'sky', 'sun']
-def quessed(word_list):  # выбирает случайное слово из списка
-    quesses = ''
+def play_game():
+    word_list = ['apple', 'ball', 'cat', 'dog', 'elephant', 'giraffe', 'house', 'tree', 'water', 'mountain', 'river', 'lake', 'cloud', 'sky', 'sun']
     word = random.choice(word_list)
-    print('Подсказка: ваше слово состоит из '+str(len(word))+' букв. Введите ваши предположения.')
-    while quesses != word: # Пока загаданное слово != 1 выбранному из списка
-        print('Введите букву')
-        quesses += input()
+    print('The word has ' + str(len(word)) + ' letters. Enter your guesses.')
+
+    guesses = ''
+    attempt = 1
+    while attempt <= 5:
+        print('Enter a letter:')
+        guesses += input()
         count = 0
-        for i in word:
-            if i in quesses:
+        for letter in word:
+            if letter in guesses:
                 count += 1
-                print(f'Отгадали букв {count}')
+                print(f'Guessed {count} letters')
         
         if count == len(word):
-            print('Вы угадали это слово!')
+            print('You guessed the word!')
             break
 
+        if attempt == 3:
+            print('Hint: The word starts with ' + word[0])
+        
+        attempt += 1
     
-word = quessed(list) # вызов функции  
+    print('Do you want to play again? Enter y or n')
+    if input() == 'y':
+        play_game()
+    else:
+        print('Goodbye!')
+
+play_game() 
 
 
