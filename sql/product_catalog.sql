11d7b757d267bea699a47c43ec94e52f110699f8
Sia <129706771+0Medsok0@users.noreply.github.com>
2023-11-13 22:40:58 +0700

    Delete sql directory

--- sql/product_catalog.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    description TEXT,
-    category VARCHAR(255) NOT NULL,
-    stock INT NOT NULL
-);
